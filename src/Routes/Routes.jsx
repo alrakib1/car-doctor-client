62405685213380c4b65a7dac0a106e8c1ff80161
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-10-28 18:43:31 +0600

    project created

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-4xl font-bold flex justify-center">Hello world!</div>,
+    },
+  ]);
+  export default router;
